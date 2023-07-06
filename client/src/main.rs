9f3005f736fce983627b388bead46f92cd72b1ba
Dmitry Mazurin <dimas.m.19@gmail.com>
2023-07-06 13:28:41 +0100

    Update client & server

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,49 +1,115 @@
 //! # Client
-//! Client is on-edge device daemon with experementation around pipe included:
-//! - pipeline is statically defined
-//!   * sqlite -> arrow -> mycelial_server
-//! - transforms collected data into arrow recordbatch
-//! - sends data serilized via arrow-ipc to mycelial server endpoint
-
+//! - communicates with server, grabs and persists pipe configs: 
+//!     - dumb server endpoint polling
+//!     - server dumbly returns all existing pipes
+//! - schedules and runs pipes
 use std::time::Duration;
+
 use clap::Parser;
-use exp2::{sqlite::Sqlite, channel::channel, arrow::ToArrow, Section, stub::Stub, net::mycelial::Mycelial, black_hole::BlackHole};
+use exp2::dynamic_pipe::{registry::{Registry, Constructor}, scheduler::Scheduler, section_impls::{sqlite, mycelial_net}, config::{Value, Config}, section};
+use serde::Deserialize;
+use tokio::time::sleep;
+use base64::engine::{general_purpose::STANDARD as BASE64, Engine};
 
 #[derive(Parser)]
 struct CLI {
     /// Server endpoint
-    #[clap(short, long, env = "ENDPOINT", default_value = "http://localhost:8080/ingestion")]
+    #[clap(short, long, env = "ENDPOINT", default_value = "http://localhost:8080/pipe/configs")]
     endpoint: String,
 
     /// Server authorization token
     #[clap(short, long, env = "ENDPOINT_TOKEN")]
     token: String,
+}
 
-    /// Sqlite path
-    #[clap(short, long, env = "SQLITE_PATH")]
-    sqlite_path: String,
+/// Setup & populate registry
+fn setup_registry() -> Registry {
+    let arr: &[(&str, Constructor)] = &[
+        ("sqlite", sqlite::constructor),
+        ("mycelial_net", mycelial_net::constructor)
+    ];
+    arr
+        .into_iter()
+        .fold(Registry::new(), |mut acc, &(section_name, constructor)| {
+            acc.register_section(section_name, constructor);
+            acc
+        })
+}
 
+#[derive(Debug)]
+struct Client {
+    endpoint: String,
+    token: String,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct RawConfig {
+    id: u64,
+    raw_config: String
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Configs {
+    configs: Vec<RawConfig>
+}
+
+impl Client {
+    fn new(endpoint: impl Into<String>, token: impl Into<String>) -> Self {
+        Self {
+            endpoint: endpoint.into(),
+            token: token.into(),
+        }
+    }
+    async fn get_configs(&self) -> Result<Vec<RawConfig>, section::Error> {
+        let client = reqwest::Client::new();
+        let url = format!("{}/pipe/configs", self.endpoint.as_str());
+        let configs: Configs = client.get(url)
+            .header("Authorization", self.basic_auth())
+            .send()
+            .await?
+            .json()
+            .await?;
+        Ok(configs.configs)
+    }
+
+    fn basic_auth(&self) -> String {
+        format!("Basic {}", BASE64.encode(format!("{}:", self.token)))
+    }
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let cli = CLI::try_parse()?;
+    let client = Client::new(cli.endpoint, cli.token);
+    let scheduler = Scheduler::new(setup_registry()).spawn();
 
-    let sqlite = Sqlite::new(cli.sqlite_path, "SELECT * FROM test", Duration::from_secs(5));
-    let (from_sqlite, to_arrow) = channel(1);
-    let sqlite_handle = tokio::spawn(async { sqlite.start(Stub::<()>::new(), from_sqlite).await });
-
-    let arrow_transform = ToArrow::new();
-    let (from_arrow, to_mycelial_net) = channel(1);
-    let arrow_handle = tokio::spawn(async { arrow_transform.start(to_arrow, from_arrow).await });
-
-    let mycelial_net = Mycelial::new(cli.endpoint, cli.token);
-    let mycelial_net_handle = tokio::spawn(async { mycelial_net.start(to_mycelial_net, BlackHole::new())}.await );
-
-    sqlite_handle.await.ok();
-    arrow_handle.await.ok();
-    mycelial_net_handle.await.ok();
-
-    Ok(())
-
+    loop {
+        match client.get_configs().await {
+            Ok(raw_configs) => {
+                let res = raw_configs
+                    .iter()
+                    .map(|raw_conf| {
+                        match Config::try_from_json(&raw_conf.raw_config) {
+                            Ok(c) => Ok((raw_conf.id, c)),
+                            Err(e) => Err(e)
+                        }
+                    })
+                    .collect::<Result<Vec<_>, _>>();
+                if let Ok(configs) = res {
+                    for (id, config) in configs {
+                        if let Err(e) = scheduler.add_pipe(id, config).await {
+                            println!("failed to schedule pipe: {:?}", e);
+                        }
+                    }
+                } else {
+                    println!("raw_configs: {:?}", raw_configs);
+                    println!("failed to parse configs: {:?}", res);
+                }
+            },
+            Err(e) => {
+                println!("failed to contact server: {:?}", e);
+            }
+        };
+        sleep(Duration::from_secs(5)).await;
+    }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,8 +1,19 @@
 //! Mycelial server
 use arrow::ipc::reader::StreamReader;
-use axum::{extract::BodyStream, response::IntoResponse, routing::post, Router, Server};
+use axum::{
+    extract::BodyStream,
+    headers::{authorization::Basic, Authorization},
+    http::{Method, Request, Uri},
+    middleware::{self, Next},
+    response::{IntoResponse, Response},
+    routing::{get, post},
+    Router, Server, TypedHeader, Json,
+};
+use chrono::Utc;
 use clap::Parser;
 use futures::StreamExt;
+use serde::Serialize;
+use serde_json::json;
 use std::net::SocketAddr;
 
 #[derive(Parser)]
@@ -11,11 +22,12 @@ struct CLI {
     listen_addr: String,
 }
 
+// FIXME: full body accumulation
 async fn ingestion(mut body: BodyStream) -> impl IntoResponse {
     let mut buf = vec![];
     while let Some(chunk) = body.next().await {
         buf.append(&mut chunk.unwrap().to_vec());
-    };
+    }
     let reader = StreamReader::try_new(buf.as_slice(), None).unwrap();
     for record_batch in reader {
         println!("got new record batch:\n{:?}", record_batch);
@@ -23,10 +35,83 @@ async fn ingestion(mut body: BodyStream) -> impl IntoResponse {
     ""
 }
 
+#[derive(Serialize)]
+struct Configs {
+    configs: Vec<RawConfig>
+}
+
+#[derive(Serialize)]
+struct RawConfig {
+    id: u64,
+    raw_config: String,
+}
+
+async fn get_pipe_configs() -> Json<Configs>{
+    let configs = Configs {
+        configs: vec![
+            RawConfig{
+                id: 1, 
+                raw_config: serde_json::to_string(
+                    &json!({
+                        "section": [
+                            {
+                                "name": "sqlite",
+                                "path": "/tmp/sqlite",
+                                "query": "select * from test",
+                            },
+                            {
+                                "name": "mycelial_net",
+                                "endpoint": "http://localhost:8080/ingestion",
+                                "token": "mycelial_net_token"
+                            }
+                        ]
+                    })
+                ).unwrap()
+            }
+        ]
+    };
+    Json(configs)
+}
+
+// log response middleware
+async fn log<B>(
+    method: Method,
+    uri: Uri,
+    maybe_auth: Option<TypedHeader<Authorization<Basic>>>,
+    request: Request<B>,
+    next: Next<B>,
+) -> Response {
+    let timestamp = Utc::now();
+    let response = next.run(request).await;
+    let request_time_ms = Utc::now()
+        .signed_duration_since(timestamp)
+        .num_milliseconds();
+
+    let token = match maybe_auth.as_ref() {
+        None => "",
+        Some(TypedHeader(Authorization(basic))) => basic.username(),
+    };
+    // FIXME: do not log token
+    let log = json!({
+        "token": token,
+        "timestamp": timestamp,
+        "request_time_ms": request_time_ms,
+        "method": method.as_str(),
+        "status_code": response.status().as_u16(),
+        "path": uri.path(),
+    });
+    println!("{}", log);
+
+    response
+}
+
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let cli = CLI::try_parse()?;
-    let router = Router::new().route("/ingestion", post(ingestion));
+    let router = Router::new()
+        .route("/ingestion", post(ingestion))
+        .route("/pipe/configs", get(get_pipe_configs))
+        .layer(middleware::from_fn(log));
 
     let addr: SocketAddr = cli.listen_addr.as_str().parse()?;
     Server::bind(&addr)
