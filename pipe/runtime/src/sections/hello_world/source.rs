751f5a805c9c3145adc4f298341b7f67b55d3f17
Alex Wilson <alex@mycelial.com>
2023-10-02 16:11:59 -0400

    add source test

--- pipe/runtime/src/sections/hello_world/source.rs
+++ pipe/runtime/src/sections/hello_world/source.rs
@@ -110,3 +110,51 @@ where
 pub fn constructor<S: State>(_config: &Map) -> Result<Box<dyn DynSection<S>>, SectionError> {
     Ok(Box::new(HelloWorld::new()))
 }
+
+#[cfg(test)]
+mod tests {
+    use section::dummy::DummySectionChannel;
+    use stub::Stub;
+
+    use super::*;
+
+    type StdError = Box<dyn std::error::Error + Send + Sync + 'static>;
+    use tokio::sync::mpsc::{Receiver, Sender};
+    use tokio_stream::wrappers::ReceiverStream;
+    use tokio_util::sync::PollSender;
+
+    pub fn channel<T>(buf_size: usize) -> (PollSender<T>, ReceiverStream<T>)
+    where
+        T: Send + 'static,
+    {
+        let (tx, rx): (Sender<T>, Receiver<T>) = tokio::sync::mpsc::channel(buf_size);
+        (PollSender::new(tx), ReceiverStream::new(rx))
+    }
+
+    #[tokio::test]
+    async fn test_source() -> Result<(), StdError> {
+        let hello_world_source = HelloWorld::new();
+
+        let input = Stub::<Message, StdError>::new();
+
+        let (output, mut rx) = channel(1);
+        let output = output.sink_map_err(|_| "chan closed".into());
+
+        let section_chan = DummySectionChannel::new();
+
+        let hello_world_source_section = hello_world_source.start(input, output, section_chan);
+        let handle = tokio::spawn(hello_world_source_section);
+
+        let out = rx.next().await.unwrap();
+        assert_eq!(out.origin, "hello world");
+
+        let expected_record_batch : RecordBatch = HelloWorldPayload {
+            message: "Hello, World!".to_string(),
+        }.try_into()?;
+
+        assert_eq!(out.payload, expected_record_batch);
+
+        handle.abort();
+        Ok(())
+    }
+}
