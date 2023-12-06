9a1a3298dc378e2a6cf876c83b2b3606c084dc53
Alex Wilson <alex@mycelial.com>
2023-12-06 12:58:11 -0500

    wip

--- pipe/section/section_impls/excel_connector/src/lib.rs
+++ pipe/section/section_impls/excel_connector/src/lib.rs
@@ -1,11 +1,11 @@
 use chrono::NaiveDateTime;
-use section::Message as _Message;
+use section::message::{Ack, Chunk, Column, DataFrame, DataType, Message, Value};
 use std::{fmt::Display, sync::Arc};
 
 pub mod source;
 
 type StdError = Box<dyn std::error::Error + Send + Sync + 'static>;
-pub type Message = _Message<ExcelPayload>;
+// pub type Message = _Message<ExcelPayload>;
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct ExcelPayload {
@@ -20,6 +20,59 @@ pub struct ExcelPayload {
 
     /// offset
     pub offset: i64,
+}
+
+pub(crate) struct TableColumn {
+    name: Arc<str>,
+    data_type: DataType,
+    nullable: bool,
+}
+pub(crate) struct NewExcelPayload {
+    columns: Arc<[TableColumn]>,
+    values: Vec<Vec<Value>>,
+}
+
+impl DataFrame for NewExcelPayload {
+    fn columns(&self) -> Vec<Column<'_>> {
+        self.columns
+            .iter()
+            .zip(self.values.iter())
+            .map(|(col, column)| {
+                Column::new(col.name.as_ref(), Box::new(column.iter().map(Into::into)))
+            })
+            .collect()
+    }
+}
+
+pub struct ExcelMessage {
+    origin: Arc<str>,
+    payload: Option<Box<dyn DataFrame>>,
+    ack: Option<Ack>,
+}
+
+impl ExcelMessage {
+    fn new(origin: Arc<str>, payload: NewExcelPayload, ack: Option<Ack>) -> Self {
+        Self {
+            origin,
+            payload: Some(Box::new(payload)),
+            ack,
+        }
+    }
+}
+
+impl Message for ExcelMessage {
+    fn origin(&self) -> &str {
+        &self.origin.as_ref()
+    }
+
+    fn next(&mut self) -> section::message::Next<'_> {
+        let v = self.payload.take().map(Chunk::DataFrame);
+        Box::pin(async move { Ok(v) })
+    }
+
+    fn ack(&mut self) -> section::message::Ack {
+        self.ack.take().unwrap_or(Box::pin(async {}))
+    }
 }
 
 // FIXME: numeric?
