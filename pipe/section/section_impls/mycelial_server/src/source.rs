fbffe9c1af53f8da85a8cc9a4a2153f2c3cdf493
Dmitry Mazurin <dimas.m.19@gmail.com>
2023-12-21 12:47:04 +0000

    fmt

--- pipe/section/section_impls/mycelial_server/src/source.rs
+++ pipe/section/section_impls/mycelial_server/src/source.rs
@@ -1,28 +1,34 @@
 //! Mycelial Net
 
+use arrow::{
+    array::AsArray,
+    datatypes::{
+        DataType as ArrowDataType, Date64Type, Decimal128Type, Float32Type, Float64Type, Int16Type,
+        Int32Type, Int64Type, Int8Type, Schema, Time64MicrosecondType, TimestampMicrosecondType,
+        UInt16Type, UInt32Type, UInt64Type, UInt8Type,
+    },
+    ipc::reader::StreamReader,
+    record_batch::RecordBatch as ArrowRecordBatch,
+};
 use reqwest::Client;
 use section::{
-    decimal,
-    futures::{self, stream::FusedStream, Stream, StreamExt, Sink, SinkExt, FutureExt},
     command_channel::{Command, SectionChannel, WeakSectionChannel},
+    decimal,
+    futures::{self, stream::FusedStream, FutureExt, Sink, SinkExt, Stream, StreamExt},
+    message::{Ack, Chunk, Column, DataFrame, DataType, Message, ValueView},
+    section::Section,
     state::State,
-    section::Section, SectionMessage, SectionError, SectionFuture, message::{DataFrame, ValueView, DataType, Column, Message, Ack, Chunk}
-};
-use arrow::{
-    ipc::reader::StreamReader,
-    datatypes::{
-        DataType as ArrowDataType,
-        Field,
-        Schema, TimeUnit, UnionFields, UnionMode,
-        DECIMAL128_MAX_PRECISION, DECIMAL_DEFAULT_SCALE, Int8Type, Int16Type, UInt8Type, UInt16Type, UInt32Type, UInt64Type, Float32Type, Float64Type, Int64Type, Int32Type, BooleanType, Time64MicrosecondType, Date64Type, TimestampMicrosecondType, Decimal128Type,
-    },
-    record_batch::RecordBatch as ArrowRecordBatch, array::AsArray,
+    SectionError, SectionFuture, SectionMessage,
 };
 use tokio::time::{Instant, Interval};
 
 use base64::engine::{general_purpose::STANDARD as BASE64, Engine};
-use std::{pin::{pin, Pin}, sync::Arc, io::Cursor};
 use std::time::Duration;
+use std::{
+    io::Cursor,
+    pin::{pin, Pin},
+    sync::Arc,
+};
 
 #[derive(Debug)]
 pub struct Mycelial {
@@ -38,157 +44,209 @@ pub struct Mycelial {
 
 // Wrap around arrow record batch, which implements dataframe
 #[derive(Debug)]
-struct RecordBatch{
+struct RecordBatch {
     inner: ArrowRecordBatch,
-    schema: Arc<Schema>
+    schema: Arc<Schema>,
 }
 
 impl DataFrame for RecordBatch {
     fn columns(&self) -> Vec<section::message::Column<'_>> {
-        self
-            .schema
+        self.schema
             .fields()
             .iter()
             .zip(self.inner.columns())
             .map(|(field, column)| {
-                let (dt, iter): (DataType, Box<dyn Iterator<Item=ValueView> + Send>) = match field.data_type() {
+                let (dt, iter): (DataType, Box<dyn Iterator<Item = ValueView> + Send>) = match field
+                    .data_type()
+                {
                     ArrowDataType::Int8 => {
                         let arr = column.as_primitive::<Int8Type>();
                         (
                             DataType::I8,
-                            Box::new(arr.iter().map(|val| val.map(ValueView::I8).unwrap_or(ValueView::Null)))
+                            Box::new(
+                                arr.iter()
+                                    .map(|val| val.map(ValueView::I8).unwrap_or(ValueView::Null)),
+                            ),
                         )
-                    },
+                    }
                     ArrowDataType::Int16 => {
                         let arr = column.as_primitive::<Int16Type>();
                         (
                             DataType::I16,
-                            Box::new(arr.iter().map(|val| val.map(ValueView::I16).unwrap_or(ValueView::Null)))
+                            Box::new(
+                                arr.iter()
+                                    .map(|val| val.map(ValueView::I16).unwrap_or(ValueView::Null)),
+                            ),
                         )
-                    },
+                    }
                     ArrowDataType::Int32 => {
                         let arr = column.as_primitive::<Int32Type>();
                         (
                             DataType::I32,
-                            Box::new(arr.iter().map(|val| val.map(ValueView::I32).unwrap_or(ValueView::Null)))
+                            Box::new(
+                                arr.iter()
+                                    .map(|val| val.map(ValueView::I32).unwrap_or(ValueView::Null)),
+                            ),
                         )
-                    },
+                    }
                     ArrowDataType::Int64 => {
                         let arr = column.as_primitive::<Int64Type>();
                         (
                             DataType::I64,
-                            Box::new(arr.iter().map(|val| val.map(ValueView::I64).unwrap_or(ValueView::Null)))
+                            Box::new(
+                                arr.iter()
+                                    .map(|val| val.map(ValueView::I64).unwrap_or(ValueView::Null)),
+                            ),
                         )
-                    },
+                    }
                     ArrowDataType::UInt8 => {
                         let arr = column.as_primitive::<UInt8Type>();
                         (
                             DataType::U8,
-                            Box::new(arr.iter().map(|val| val.map(ValueView::U8).unwrap_or(ValueView::Null)))
+                            Box::new(
+                                arr.iter()
+                                    .map(|val| val.map(ValueView::U8).unwrap_or(ValueView::Null)),
+                            ),
                         )
-                    },
+                    }
                     ArrowDataType::UInt16 => {
                         let arr = column.as_primitive::<UInt16Type>();
                         (
                             DataType::U16,
-                            Box::new(arr.iter().map(|val| val.map(ValueView::U16).unwrap_or(ValueView::Null)))
+                            Box::new(
+                                arr.iter()
+                                    .map(|val| val.map(ValueView::U16).unwrap_or(ValueView::Null)),
+                            ),
                         )
-                    },
+                    }
                     ArrowDataType::UInt32 => {
                         let arr = column.as_primitive::<UInt32Type>();
                         (
                             DataType::U32,
-                            Box::new(arr.iter().map(|val| val.map(ValueView::U32).unwrap_or(ValueView::Null)))
+                            Box::new(
+                                arr.iter()
+                                    .map(|val| val.map(ValueView::U32).unwrap_or(ValueView::Null)),
+                            ),
                         )
-                    },
+                    }
                     ArrowDataType::UInt64 => {
                         let arr = column.as_primitive::<UInt64Type>();
                         (
                             DataType::U64,
-                            Box::new(arr.iter().map(|val| val.map(ValueView::U64).unwrap_or(ValueView::Null)))
+                            Box::new(
+                                arr.iter()
+                                    .map(|val| val.map(ValueView::U64).unwrap_or(ValueView::Null)),
+                            ),
                         )
-                    },
+                    }
                     ArrowDataType::Float32 => {
                         let arr = column.as_primitive::<Float32Type>();
                         (
                             DataType::F32,
-                            Box::new(arr.iter().map(|val| val.map(ValueView::F32).unwrap_or(ValueView::Null)))
+                            Box::new(
+                                arr.iter()
+                                    .map(|val| val.map(ValueView::F32).unwrap_or(ValueView::Null)),
+                            ),
                         )
-                    },
+                    }
                     ArrowDataType::Float64 => {
                         let arr = column.as_primitive::<Float64Type>();
                         (
                             DataType::F64,
-                            Box::new(arr.iter().map(|val| val.map(ValueView::F64).unwrap_or(ValueView::Null)))
+                            Box::new(
+                                arr.iter()
+                                    .map(|val| val.map(ValueView::F64).unwrap_or(ValueView::Null)),
+                            ),
                         )
-                    },
+                    }
                     ArrowDataType::Utf8 => {
                         let arr = column.as_string::<i32>();
                         (
                             DataType::Str,
-                            Box::new(arr.iter().map(|val| val.map(ValueView::Str).unwrap_or(ValueView::Null)))
+                            Box::new(
+                                arr.iter()
+                                    .map(|val| val.map(ValueView::Str).unwrap_or(ValueView::Null)),
+                            ),
                         )
-                    },
+                    }
                     ArrowDataType::Binary => {
                         let arr = column.as_binary::<i32>();
                         (
                             DataType::Bin,
-                            Box::new(arr.iter().map(|val| val.map(ValueView::Bin).unwrap_or(ValueView::Null)))
+                            Box::new(
+                                arr.iter()
+                                    .map(|val| val.map(ValueView::Bin).unwrap_or(ValueView::Null)),
+                            ),
                         )
-                    },
+                    }
                     ArrowDataType::Boolean => {
                         let arr = column.as_boolean();
                         (
                             DataType::Bool,
-                            Box::new(arr.iter().map(|val| val.map(ValueView::Bool).unwrap_or(ValueView::Null)))
+                            Box::new(
+                                arr.iter()
+                                    .map(|val| val.map(ValueView::Bool).unwrap_or(ValueView::Null)),
+                            ),
                         )
-                    },
+                    }
                     ArrowDataType::Time64(_tu) => {
                         let arr = column.as_primitive::<Time64MicrosecondType>();
                         (
                             DataType::Time,
-                            Box::new(arr.iter().map(|val| val.map(ValueView::Time).unwrap_or(ValueView::Null)))
+                            Box::new(
+                                arr.iter()
+                                    .map(|val| val.map(ValueView::Time).unwrap_or(ValueView::Null)),
+                            ),
                         )
-                    },
+                    }
                     ArrowDataType::Date64 => {
                         let arr = column.as_primitive::<Date64Type>();
                         (
                             DataType::Date,
-                            Box::new(arr.iter().map(|val| val.map(ValueView::Date).unwrap_or(ValueView::Null)))
+                            Box::new(
+                                arr.iter()
+                                    .map(|val| val.map(ValueView::Date).unwrap_or(ValueView::Null)),
+                            ),
                         )
-                    },
+                    }
                     ArrowDataType::Timestamp(_tu, _tz) => {
                         let arr = column.as_primitive::<TimestampMicrosecondType>();
                         (
                             DataType::TimeStamp,
-                            Box::new(arr.iter().map(|val| val.map(ValueView::TimeStamp).unwrap_or(ValueView::Null)))
+                            Box::new(arr.iter().map(|val| {
+                                val.map(ValueView::TimeStamp).unwrap_or(ValueView::Null)
+                            })),
                         )
-                    },
+                    }
                     ArrowDataType::Null => {
                         let arr = column.as_primitive::<Int8Type>();
                         (
                             DataType::Null,
-                            Box::new(arr.iter().map(|_| ValueView::Null))
+                            Box::new(arr.iter().map(|_| ValueView::Null)),
                         )
-                    },
-                    ArrowDataType::Decimal128(precision, scale) => {
+                    }
+                    ArrowDataType::Decimal128(_precision, scale) => {
                         let arr = column.as_primitive::<Decimal128Type>();
                         (
                             DataType::Decimal,
-                            Box::new(arr.iter().map(|val| 
-                                val.map(|num| 
-                                        ValueView::Decimal(decimal::Decimal::from_i128_with_scale(num, *scale as _))
-                                ).unwrap_or(ValueView::Null)
-                            ))
+                            Box::new(arr.iter().map(|val| {
+                                val.map(|num| {
+                                    ValueView::Decimal(decimal::Decimal::from_i128_with_scale(
+                                        num,
+                                        *scale as _,
+                                    ))
+                                })
+                                .unwrap_or(ValueView::Null)
+                            })),
                         )
-                    },
+                    }
                     ArrowDataType::Union(_uf, _mode) => {
                         unimplemented!()
-                    },
+                    }
                     dt => panic!("unsupported arrow datatype: {:?}", dt),
                 };
                 Column::new(field.name(), dt, iter)
-        })
+            })
             .collect()
     }
 }
@@ -221,9 +279,7 @@ impl Message for Msg {
             false => {
                 let rb = self.inner[self.pos].take().unwrap();
                 self.pos += 1;
-                Box::pin(async move { 
-                    Ok(Some(Chunk::DataFrame(Box::new(rb))))
-                })
+                Box::pin(async move { Ok(Some(Chunk::DataFrame(Box::new(rb)))) })
             }
         }
     }
@@ -231,9 +287,7 @@ impl Message for Msg {
     fn ack(&mut self) -> section::message::Ack {
         self.ack.take().unwrap_or(Box::pin(async {}))
     }
-
 }
-
 
 struct IntervalStream {
     delay: Duration,
@@ -368,23 +422,26 @@ impl Mycelial {
         *offset = maybe_new_offset;
 
         let body = res.bytes().await?.to_vec();
-        let len = body.len() as u64; 
+        let len = body.len() as u64;
         let mut body = Cursor::new(body);
         let mut batches = vec![];
         while body.position() < len {
             let reader = StreamReader::try_new_unbuffered(&mut body, None).unwrap();
             for batch in reader {
                 let batch = batch?;
-                batches.push(Some(RecordBatch{ schema: batch.schema(), inner: batch }))
+                batches.push(Some(RecordBatch {
+                    schema: batch.schema(),
+                    inner: batch,
+                }))
             }
-        };
+        }
         let weak_chan = section_chan.weak_chan();
         let o = *offset;
         let msg = Msg {
             inner: batches,
             pos: 0,
             origin,
-            ack: Some( Box::pin(async move { weak_chan.ack(Box::new(o)).await })),
+            ack: Some(Box::pin(async move { weak_chan.ack(Box::new(o)).await })),
         };
         Ok(Some(Box::new(msg)))
     }
