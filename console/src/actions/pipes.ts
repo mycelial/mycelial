a367704965bab68ee472b8617a3d3fefabeb2321
Alex Wilson <alex@mycelial.com>
2024-01-23 09:53:22 -0500

    handle mycelial server correctly

--- console/src/actions/pipes.ts
+++ console/src/actions/pipes.ts
@@ -1,8 +1,13 @@
-import axios from 'axios';
-import { getId } from '../utils';
-import { PIPE_URL, edgePresets, nodePresets, headers } from '../utils/constants';
-import { Node, Position } from 'reactflow';
-import { NodeData } from '../types';
+import axios from "axios";
+import { getId } from "../utils";
+import {
+  PIPE_URL,
+  edgePresets,
+  nodePresets,
+  headers,
+} from "../utils/constants";
+import { Node, Position } from "reactflow";
+import { NodeData } from "../types";
 
 async function getPipes() {
   try {
@@ -36,14 +41,15 @@ const createPipe = async ({
   sourceNodeData,
   targetNodeData,
 }: createPipeParams) => {
-  let method = 'put';
+  let method = "put";
   if (id === 0) {
-    method = 'post';
+    method = "post";
   }
 
-  if (sourceNodeData.type === 'mycelial_server') sourceNodeData.name = 'mycelial_server_source';
-  if (targetNodeData.type === 'mycelial_server')
-    targetNodeData.name = 'mycelial_server_destination';
+  if (sourceNodeData.type === "mycelial_server")
+    sourceNodeData.name = "mycelial_server_source";
+  if (targetNodeData.type === "mycelial_server")
+    targetNodeData.name = "mycelial_server_destination";
 
   const payload = {
     configs: [
@@ -62,7 +68,7 @@ const createPipe = async ({
       data: payload,
       headers,
     });
-    if (method === 'post') {
+    if (method === "post") {
       return response.data[0];
     }
     return response.status;
@@ -72,11 +78,16 @@ const createPipe = async ({
 };
 
 const newCreatePipe = async ({ workspace_id, id, pipe }: pipeData) => {
-  let method = 'put';
+  let method = "put";
   if (id === 0 || id === undefined) {
-    method = 'post';
+    method = "post";
   }
-  // todo: Split into two pipes if there's a mycelial_server in the middle? 
+  if (pipe[0].type === "mycelial_server") {
+    pipe[0].name = "mycelial_server_source";
+  }
+  if (pipe[pipe.length - 1].type === "mycelial_server") {
+    pipe[pipe.length - 1].name = "mycelial_server_destination";
+  }
 
   const payload = {
     configs: [
@@ -94,7 +105,7 @@ const newCreatePipe = async ({ workspace_id, id, pipe }: pipeData) => {
       data: payload,
       headers,
     });
-    if (method === 'post') {
+    if (method === "post") {
       return response.data[0];
     }
     return response.status;
@@ -106,7 +117,7 @@ const newCreatePipe = async ({ workspace_id, id, pipe }: pipeData) => {
 const deletePipe = async (id: string) => {
   try {
     const response = await axios({
-      method: 'delete',
+      method: "delete",
       url: `${PIPE_URL}/${id}`,
       headers,
     });
@@ -117,8 +128,10 @@ const deletePipe = async (id: string) => {
 };
 
 const configurePipes = (pipes: pipeData[]) => {
-  let initialNodes: Partial<Node>[] = [];
+  let initialNodes: { [id: string]: Partial<Node> } = {};
   let initialEdges = [];
+
+  let nodemap: { [key: string]: string } = {};
 
   for (const pipeConfig of pipes) {
     let source = null;
@@ -135,16 +148,23 @@ const configurePipes = (pipes: pipeData[]) => {
       } = { ...nodePresets };
       const { name, ...data } = nodeData;
 
-      const id = getId();
-      node.id = id;
-      initialNodes[id] = {};
+      const key = JSON.stringify(data);
+
+      // only combine "mycelial_server" nodes -- as a kind of visual syntactic sugar.
+      if (nodemap.hasOwnProperty(key) && nodeData.type === "mycelial_server") {
+        node.id = nodemap[key];
+      } else {
+        const id = getId();
+        node.id = id;
+        nodemap[key] = id;
+        initialNodes[id] = {};
+      }
 
       node.data = nodeData;
       node.key = node.id;
       if (index === 0) {
         node.data.id = pipeId;
-      };
-
+      }
       if (node.data.source === true) {
         node.sourcePosition = Position.Right;
         edge.source = node.id;
@@ -154,13 +174,13 @@ const configurePipes = (pipes: pipeData[]) => {
         edge.target = node.id;
       }
 
-      initialNodes.push(node);
+      initialNodes[node.id] = node;
 
       if (source !== null) {
-        const e = {...edge};
+        const e = { ...edge };
         e.id = getId("edge");
         e.source = source.id || "0";
-        e.target = node.id
+        e.target = node.id;
         initialEdges.push(e);
       }
       source = node;
@@ -168,7 +188,7 @@ const configurePipes = (pipes: pipeData[]) => {
   }
 
   const data = {
-    nodes: initialNodes,
+    nodes: Object.values(initialNodes),
     edges: initialEdges,
   };
 
