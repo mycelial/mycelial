c6fd03f6efa0abf967114a7f5e99b504292c9fcf
alexandrinaw <alex@mycelial.com>
2024-02-14 15:41:27 -0500

    store client creds in same db as section store (#126)

---
+++ myceliald/migrations/20240214194737_client_creds.sql
@@ -0,0 +1,4 @@
+CREATE TABLE client_creds(
+    client_id TEXT,
+    client_secret TEXT
+);
--- myceliald/src/daemon_storage.rs
+++ myceliald/src/daemon_storage.rs
@@ -2,11 +2,13 @@
 //!
 
 use serde::{Deserialize, Serialize};
-use std::fs;
+use sqlx::{sqlite::SqliteConnectOptions, ConnectOptions, Row, SqliteConnection};
+use std::str::FromStr;
 
 pub struct Storage {
     #[allow(unused)]
     path: String,
+    connection: SqliteConnection,
 
     config: Option<Config>,
 }
@@ -21,20 +23,42 @@ pub struct Config {
 impl Storage {
     pub async fn new(path: impl Into<String>) -> Result<Self, StdError> {
         let path = path.into();
-        Ok(Self { path, config: None })
+        let mut connection = SqliteConnectOptions::from_str(path.as_str())?
+            .create_if_missing(true)
+            .connect()
+            .await?;
+        sqlx::migrate!().run(&mut connection).await?;
+        Ok(Self {
+            path,
+            connection,
+            config: None,
+        })
     }
 
-    fn read_config(&self) -> Result<Config, StdError> {
-        let data = fs::read(self.path.clone())?;
-        let text = String::from_utf8(data)?;
-        let config: Config = toml::from_str(&text)?;
-        Ok(config)
-    }
-
-    fn write_config(&self, config: &Config) -> Result<(), StdError> {
-        let text = toml::to_string(config)?;
-        std::fs::write(self.path.clone(), text)?;
+    pub async fn write_config(&mut self, config: &Config) -> Result<(), StdError> {
+        sqlx::query("INSERT INTO client_creds (client_id, client_secret) VALUES (?, ?)")
+            .bind(config.client_id.clone())
+            .bind(config.client_secret.clone())
+            .execute(&mut self.connection)
+            .await?;
         Ok(())
+    }
+
+    pub async fn read_config(&mut self) -> Result<Option<Config>, StdError> {
+        let row = sqlx::query("SELECT client_id, client_secret FROM client_creds")
+            .fetch_optional(&mut self.connection)
+            .await?;
+        match row {
+            Some(row) => {
+                let client_id: String = row.get(0);
+                let client_secret: String = row.get(1);
+                Ok(Some(Config {
+                    client_id,
+                    client_secret,
+                }))
+            }
+            None => Ok(None),
+        }
     }
 
     pub async fn store_client_creds(
@@ -45,23 +69,22 @@ impl Storage {
         self.write_config(&Config {
             client_id,
             client_secret,
-        })?;
+        })
+        .await?;
         Ok(())
     }
 
     pub async fn retrieve_client_creds(&mut self) -> Result<Option<(String, String)>, StdError> {
+        // Try to load the config if it's not already loaded
         if self.config.is_none() {
-            let config = self.read_config();
-            match config {
-                Ok(c) => {
-                    self.config = Some(c);
-                }
-                Err(_) => {
-                    return Ok(None);
-                }
+            if let Ok(Some(c)) = self.read_config().await {
+                self.config = Some(c);
+            } else {
+                return Ok(None);
             }
         }
 
+        // Assuming at this point self.config must be Some
         let c = self.config.as_ref().unwrap();
         Ok(Some((c.client_id.clone(), c.client_secret.clone())))
     }
