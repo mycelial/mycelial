c8fdd7536d5faef5dfe992e752eae577d5ab456a
Dmitry Mazurin <dimas.m.19@gmail.com>
2024-04-30 00:15:15 +0100

    Workspaces initial

--- ui/src/components/workspaces.rs
+++ ui/src/components/workspaces.rs
@@ -3,6 +3,25 @@ use dioxus::prelude::*;
 #[component]
 pub fn Workspaces() -> Element {
     rsx! {
-        "workspaces"
+        div {
+            class: "flex pt-4",
+            div {
+                class: "w-1/12",
+            }
+            div {
+                class: "flex w-10/12 justify-between",
+                h2 {
+                    class: "flex-none content-center",
+                    font_size: "1.5em",
+                    font_weight: "bold",
+                    "Workspaces"
+                }
+                button {
+                    class: "flex-initial text-white px-4 py-2 rounded",
+                    style: "background-color: #1a237e",
+                    "ADD NEW WORKSPACE"
+                }
+            }
+        }
     }
 }
