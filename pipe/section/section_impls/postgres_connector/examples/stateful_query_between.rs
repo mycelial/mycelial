a3c1c7f29a81231bf7139b5ced19f7f712a045f9
Dmitriy Mazurin <dimas.m.19@gmail.com>
2024-06-10 20:46:00 +0100

    Stateful query update (#173)
    Update stateful queries

---
+++ pipe/section/section_impls/postgres_connector/examples/stateful_query_between.rs
@@ -0,0 +1,71 @@
+use std::time::{Duration, Instant};
+
+use section::{
+    dummy::DummySectionChannel,
+    futures::{self, SinkExt, StreamExt},
+    message::{Chunk, ValueView},
+    section::Section,
+    SectionError,
+};
+use stub::Stub;
+
+#[tokio::main]
+async fn main() -> Result<(), SectionError> {
+    tracing_subscriber::fmt::init();
+
+    let pg_src = postgres_connector::source::Postgres::new(
+        "postgres://user:password@localhost:5432/postgres",
+        "public",
+        "select * from test where id between $id::i64 + 1 and $id::i64 + 1000000 order by id",
+        Duration::from_secs(5),
+    )?;
+    let (tx, mut rx) = futures::channel::mpsc::channel(1);
+    let tx = tx.sink_map_err(|_| "send error".into());
+    let handle = tokio::spawn(pg_src.start(Stub::<()>::new(), tx, DummySectionChannel::new()));
+    let mut start = Instant::now();
+    while let Some(mut msg) = rx.next().await {
+        let mut count = 0;
+        let mut min = i64::MAX;
+        let mut max = 0;
+        while let Ok(Some(chunk)) = msg.next().await {
+            match chunk {
+                Chunk::DataFrame(df) => {
+                    let (new_min, new_max, new_count) = df
+                        .columns()
+                        .into_iter()
+                        .filter(|col| col.name() == "id")
+                        .fold(
+                            (min, max, count),
+                            |(mut min, mut max, mut count), mut col| {
+                                for next in col.by_ref() {
+                                    match next {
+                                        ValueView::I64(val) => {
+                                            count += 1;
+                                            min = min.min(val);
+                                            max = max.max(val);
+                                        }
+                                        _ => panic!("unexpected value: {next}",),
+                                    };
+                                }
+                                (min, max, count)
+                            },
+                        );
+                    count = new_count;
+                    min = new_min;
+                    max = new_max;
+                }
+                _ => panic!("unexpected: {:?}", chunk),
+            };
+        }
+        tracing::info!(
+            "{} in {}ms min_id: {}, max_id: {}",
+            count,
+            start.elapsed().as_millis(),
+            min,
+            max
+        );
+        start = Instant::now();
+    }
+    tracing::error!("{:?}", handle.await?);
+    Ok(())
+}
