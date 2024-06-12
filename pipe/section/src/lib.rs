a992ed83bdaf11e8ee2d66976deaf60ecd630de8
Dmitriy Mazurin <dimas.m.19@gmail.com>
2024-06-12 19:43:24 +0100

    Add s3 destination

--- pipe/section/src/lib.rs
+++ pipe/section/src/lib.rs
@@ -17,3 +17,19 @@ pub type SectionError = Box<dyn std::error::Error + Send + Sync + 'static>;
 pub type SectionFuture =
     Pin<Box<dyn std::future::Future<Output = Result<(), SectionError>> + Send + 'static>>;
 pub type SectionMessage = Box<dyn crate::message::Message>;
+
+
+pub mod prelude {
+    pub use crate::{
+        SectionError,
+        SectionFuture,
+        SectionMessage,
+        section::Section,
+        message::{Message, Chunk, Column, Value, ValueView, DataFrame, DataType},
+        command_channel::{SectionChannel, WeakSectionChannel, RootChannel, Command},
+        futures::{self, Future, Sink, Stream, SinkExt, StreamExt, FutureExt},
+        uuid,
+        async_trait,
+        decimal,
+    };
+}
