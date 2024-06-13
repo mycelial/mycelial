3cf5d3aad20550f3f7e8f1c216c9395737a4c61a
Dmitriy Mazurin <dimas.m.19@gmail.com>
2024-06-13 11:59:00 +0100

    Add test for chunk buffer. Add s3 to myceliald runtime

--- myceliald/src/runtime.rs
+++ myceliald/src/runtime.rs
@@ -87,6 +87,10 @@ fn setup_registry<S: SectionChannel>() -> Registry<S> {
             "origin_time_nanos_transform",
             constructors::origin_transform::time_nanos_ctor,
         ),
+        (
+            "s3_destination",
+            constructors::s3::destination_ctor,
+        )
         //("sqlite_physical_replication_destination", sqlite_physical_replication::destination::constructor),
         //("sqlite_physical_replication_source", sqlite_physical_replication::source::constructor),
     ];
