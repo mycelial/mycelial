83d4b734cbde5ff3abbab480013c4544b4f96c15
Dmitriy Mazurin <dimas.m.19@gmail.com>
2024-09-18 13:21:55 +0100

    Drop constructors

--- myceliald/src/constructors/inspect.rs
+++
@@ -1,8 +0,0 @@
-use pipe::{config::Map, types::DynSection};
-use section::prelude::*;
-
-pub fn inspect_ctor<S: SectionChannel>(
-    _config: &Map,
-) -> Result<Box<dyn DynSection<S>>, SectionError> {
-    Ok(Box::new(inspect::Inspect::default()))
-}
